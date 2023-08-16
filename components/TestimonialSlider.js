08892e228872cee15820e7b45ad767361782569e
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-08-16 15:56:38 +0200

    Initialzed app with Homepage

---
+++ components/TestimonialSlider.js
@@ -0,0 +1,30 @@
+// testimonial data
+const testimonialData = [
+  {
+    image: "/t-avt-1.png",
+    name: "Manal Mohamed",
+    position: "Customer",
+    message:
+      "I had the pleasure of working with Ahmed on a complex web application project",
+  },
+  {
+    image: "/t-avt-2.png",
+    name: "Mohamed Galal",
+    position: "Backend-developer",
+    message:
+      "Ahmed is a top-notch React developer who consistently exceeded our expectations. Their proficiency in crafting efficient and maintainable React components greatly contributed to the success of our project",
+  },
+  {
+    image: "/t-avt-3.png",
+    name: "Ahmed Nabout",
+    position: "Customer",
+    message:
+      "I had the privilege of collaborating with Ahmedd on a critical React-driven application, and I must say, they are a true React virtuoso",
+  },
+];
+
+const TestimonialSlider = () => {
+  return <div>Testimonial Slider</div>;
+};
+
+export default TestimonialSlider;
